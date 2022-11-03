9776d5f5c70cf044f2f09545ef0b5094e93ad1c3
Rohit <RohitSinghEmail@protonmail.com>
2022-11-03 22:33:49 +0530

    anchor nft mint

---
+++ client/main.py
@@ -0,0 +1,13 @@
+from solana.transaction import Transaction
+from solana.keypair import Keypair
+from solana.publickey import PublicKey
+from anchorpy import Provider
+from client.instructions import mint
+
+
+NFT_TITLE = 'Rohit Special NFT!';
+NFT_SYMBOL = 'RSN'
+NFT_URL = ''
+
+def main():
+    pass
