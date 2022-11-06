3874747fecaef8a5328297105488cb303261a575
Rohit <RohitSinghEmail@protonmail.com>
2022-11-06 16:11:42 +0530

    minor change

--- client/main2.py
+++ client/main2.py
@@ -15,11 +15,12 @@ from anchorpy import Wallet
 
 from .instructions.mint import MintAccounts, MintArgs, mint
 
-
-TOKEN_METADATA_PROGRAM_ID = PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s") # From docs
+TOKEN_METADATA_PROGRAM_ID = PublicKey(
+    "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s")  # From docs
 METADATA_TITLE = "Rohit Special Token"
 METADATA_URL = "https://raw.githubusercontent.com/RohitSingh107/anchor-nft-mint/main/assets/example.json"
 METADATA_SYMBOL = "RST"
+
 
 async def main():
 
@@ -27,30 +28,31 @@ async def main():
 
     mint_keypair = Keypair()
 
-    metadata_address = PublicKey.find_program_address(seeds= ["metadata".encode(encoding= 'UTF-8') , TOKEN_METADATA_PROGRAM_ID.__bytes__(), mint_keypair.public_key.__bytes__()], program_id= TOKEN_METADATA_PROGRAM_ID)[0]
+    metadata_address = PublicKey.find_program_address(seeds=["metadata".encode(
+        encoding='UTF-8'), TOKEN_METADATA_PROGRAM_ID.__bytes__(), mint_keypair.public_key.__bytes__()], program_id=TOKEN_METADATA_PROGRAM_ID)[0]
     print(f"metadata_address is {metadata_address}")
 
+    metadata_master_edition_address = PublicKey.find_program_address(seeds=["metadata".encode(encoding='UTF-8'), TOKEN_METADATA_PROGRAM_ID.__bytes__(
+    ), mint_keypair.public_key.__bytes__(), "edition".encode(encoding='UTF-8')], program_id=TOKEN_METADATA_PROGRAM_ID)[0]
 
-    metadata_master_edition_address = PublicKey.find_program_address(seeds= ["metadata".encode(encoding= 'UTF-8') , TOKEN_METADATA_PROGRAM_ID.__bytes__(), mint_keypair.public_key.__bytes__(), "edition".encode(encoding= 'UTF-8')], program_id= TOKEN_METADATA_PROGRAM_ID)[0]
+    token_address = get_associated_token_address(
+        wallet.public_key, mint_keypair.public_key)
 
-
-
-    token_address = get_associated_token_address(wallet.public_key, mint_keypair.public_key)
-
-    mintArgs = MintArgs(metadata_title= METADATA_TITLE, metadata_symbol= METADATA_SYMBOL, metadata_url= METADATA_URL) 
+    mintArgs = MintArgs(metadata_title=METADATA_TITLE,
+                        metadata_symbol=METADATA_SYMBOL, metadata_url=METADATA_URL)
 
     accounts = MintAccounts(
-            metadata= metadata_address,
-            master_edition= metadata_master_edition_address,
-            mint = mint_keypair.public_key,
-            token_account= token_address,
-            mint_authority= wallet.public_key,
-            rent = SYSVAR_RENT_PUBKEY,
-            system_program= system_program.SYS_PROGRAM_ID,
-            token_program= TOKEN_PROGRAM_ID,
-            associated_token_program= ASSOCIATED_TOKEN_PROGRAM_ID,
-            token_metadata_program= TOKEN_METADATA_PROGRAM_ID
-            )
+        metadata=metadata_address,
+        master_edition=metadata_master_edition_address,
+        mint=mint_keypair.public_key,
+        token_account=token_address,
+        mint_authority=wallet.public_key,
+        rent=SYSVAR_RENT_PUBKEY,
+        system_program=system_program.SYS_PROGRAM_ID,
+        token_program=TOKEN_PROGRAM_ID,
+        associated_token_program=ASSOCIATED_TOKEN_PROGRAM_ID,
+        token_metadata_program=TOKEN_METADATA_PROGRAM_ID
+    )
 
     inx = mint(mintArgs, accounts)
 
@@ -61,12 +63,10 @@ async def main():
     txn = Transaction().add(inx)
     txn.sign(wallet.payer, mint_keypair)
     res = await client.send_transaction(txn, wallet.payer, mint_keypair)
-    await client.confirm_transaction(res.value, commitment= Finalized)
+    await client.confirm_transaction(res.value, commitment=Finalized)
     print(res)
 
-    await client.close()  
+    await client.close()
 
 if __name__ == '__main__':
     asyncio.run(main())
-
-
